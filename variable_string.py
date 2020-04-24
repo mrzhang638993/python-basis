e5c838d3c04d3e65070554f2f5c1aefd99f2cb45
mrzhang638993 <1446719386@qq.com>
2020-04-24 21:43:00 +0800

    增加字符串和变量的使用

---
+++ variable_string.py
@@ -0,0 +1,49 @@
+x=3  #代码注释
+print(x)
+loveyou1234=1234
+#520baby=420  变量名称不合法
+幸运数=588
+print(幸运数)
+
+
+name="小甲鱼"
+print(name)
+name="老乌龟"
+print(name)
+
+
+x=3
+y=5
+y=x
+print(y)
+
+x=y=3
+print(x,y)
+
+x=3  # 执行变量x,y之间的变量交换操作
+y=5
+temp=y
+y=x
+x=temp
+
+
+x=3
+y=5
+x,y=y,x   #  变量x,y之间的变量交换
+print(x,y)
+
+
+#####下面是字符串的操作
+# ‘’单引号的字符串
+print('i love china')
+# "" 双引号包裹的字符串
+print("let's go")
+# 显示双引号
+print('"life is  short ,you need  python"')
+##包含单引号和双引号的，比较复杂的话，推荐使用转义字符,复杂的单引号和双引号之间的交互推荐使用转义字符
+print('\"life is  short,let\'s study python')
+# 打印换行
+print("i love python\ni love fishc.com.cn")
+
+
+
