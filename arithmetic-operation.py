ee0da1d2ca208a6808a0dd6cd9c9182b6418400a
mrzhang638993 <1446719386@qq.com>
2020-05-24 08:27:23 +0800

    修改对应的模数运算的逻辑

--- arithmetic-operation.py
+++ arithmetic-operation.py
@@ -45,6 +45,7 @@ class Try_int(int):
     def __sub__(self, other):
         return int.__add__(self, other)
         # 对应的会出现无穷递归操作实现的，会抛出递归异常操作的
+        # 魔法方法对应的会出现无限递归操作的
         # return self - other
 
 
@@ -52,3 +53,22 @@ c = Try_int(4)
 d = Try_int(6)
 print(c + d)
 print(c - d)
+
+
+class Try_int1(int):
+    def __add__(self, other):
+        return int(self) + int(other)
+        # return self + other 注意这样的操作对应的会出现无穷的递归操作的，会出现异常的
+
+    def __sub__(self, other):
+        return int(self) + int(other)
+        # 对应的会出现无穷递归操作实现的，会抛出递归异常操作的
+        # 魔法方法对应的会出现无限递归操作的
+        # return self - other
+
+
+# 对象调用的话会产生递归操作的，简单的+，-。*，/ 对应的不会产生递归运算操作逻辑的
+e = Try_int1(6)
+f = Try_int1(8)
+print(e + f)
+print(e - f)
